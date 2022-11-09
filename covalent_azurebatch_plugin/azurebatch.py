403151825b0fa746df7c5c1704edffbd51141bde
Faiyaz Hasan <faiyaz@agnostiq.ai>
2022-11-09 17:28:35 -0500

    trying to activate precommit

--- covalent_azurebatch_plugin/azurebatch.py
+++ covalent_azurebatch_plugin/azurebatch.py
@@ -52,6 +52,41 @@ COVALENT_EXEC_BASE_URI = ""
 class AzureBatchExecutor(RemoteExecutor):
     """Microsoft Azure Batch Executor."""
 
+    def __init__(
+        self,
+        tenant_id: str = None,
+        client_id: str = None,
+        client_secret: str = None,
+        batch_account_url: str = None,
+        storage_account_name: str = None,
+        storage_account_domain: str = None,
+        pool_id: str = None,
+        job_id: str = None,
+        retries: int = None,
+        time_limit: float = None,
+        cache_dir: str = None,
+        poll_freq: int = None,
+    ) -> None:
+        """Azure Batch executor initialization."""
+        self.tenant_id = tenant_id or get_config("executors.azurebatch.tenant_id")
+        self.client_id = client_id or get_config("executors.azurebatch.client_id")
+        self.client_secret = client_secret or get_config("executors.azurebatch.client_secret")
+        self.batch_account_url = batch_account_url or get_config(
+            "executors.azurebatch.batch_account_url"
+        )
+        self.storage_account_name = storage_account_name or get_config(
+            "executors.azurebatch.storage_account_name"
+        )
+        self.storage_account_domain = storage_account_domain or get_config(
+            "executors.azurebatch.storage_account_domain"
+        )
+        self.pool_id = pool_id or get_config("executors.azurebatch.pool_id")
+        self.job_id = job_id or get_config("executors.azurebatch.job_id")
+        self.retries = retries or get_config("executors.azurebatch.retries")
+        self.time_limit = time_limit or get_config("executors.azurebatch.time_limit")
+        self.cache_dir = cache_dir or get_config("executors.azurebatch.cache_dir")
+        self.poll_freq = poll_freq or get_config("executors.azurebatch.poll_freq")
+
     # TODO - Add return type
     def _validate_credentials(self, raise_exception: bool = True):
         """Validate user-specified Microsoft Azure credentials or environment variables (configured before starting the server). Note: credentials passed should be those of a service principal rather than a developer account.
